d166d492f520f380173bc3e6b6bd563454dec382
Victor <VictorWiegand@BoteloVictor.onmicrosoft.com>
2023-12-01 11:05:34 +0100

    next.js upload

---
+++ src/app/dashboard/customers/page.js
@@ -0,0 +1,5 @@
+import '../../globals.css'
+
+export default function Page(){
+    return <p>SHREX CUSTOMERS</p>
+}
---
+++ src/app/dashboard/page.js
@@ -0,0 +1,15 @@
+import '../globals.css'
+
+export default function Page(){
+    return (    <main>
+    <p>SEX</p>
+    <p>SEX</p>
+    <p>SEX</p>
+    <p>SEX</p>
+    <p>SEX</p>
+    <p>SEX</p>
+    <p>SEX</p>
+    <p>SEX</p>
+    </main>
+    )
+}
